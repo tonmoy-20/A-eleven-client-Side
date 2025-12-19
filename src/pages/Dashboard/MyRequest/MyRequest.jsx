b1945329ee9e3275379f29fd4ff4473ce150e526
Tonmoy <tonmoybiswas20602@gmail.com>
2025-12-19 19:59:27 +0600

    number of page added

--- src/pages/Dashboard/MyRequest/MyRequest.jsx
+++ src/pages/Dashboard/MyRequest/MyRequest.jsx
@@ -3,13 +3,23 @@ import useAxiosSecure from "../../../hooks/useAxiosSecqure";
 
 const MyRequest = () => {
   const [myRequest, setMyRequest] = useState([]);
+
+  const [totalRequest, setTotalRequest] = useState(0);
+
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
   const axiosSecure = useAxiosSecure();
 
   useEffect(() => {
-    axiosSecure.get("/my-request").then((res) => {
-      console.log(res.data);
+    axiosSecure.get("/my-request?page=0&size=10").then((res) => {
+      setMyRequest(res.data.request);
+      setTotalRequest(res.data.totalRequest);
     });
   }, [axiosSecure]);
+  const numberOfPage = Math.ceil(totalRequest / itemsPerPage);
+  console.log(myRequest);
+  console.log(totalRequest);
+  console.log(numberOfPage);
 
   return <div>my request</div>;
 };
