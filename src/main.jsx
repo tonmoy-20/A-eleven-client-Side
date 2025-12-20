c3e652bb9025ec77e8453bbef0ba36b036b541b3
Tonmoy <tonmoybiswas20602@gmail.com>
2025-12-20 12:38:28 +0600

    payment completed

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import { RouterProvider } from "react-router";
 import AuthProvider from "./Provider/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
-  </StrictMode>
+  </>
 );
